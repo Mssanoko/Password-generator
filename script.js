da9461a1d54bbe12cccdd4c41c44a089545595d0
Leslie Lawson <lawsonjleslie@gmail.com>
2020-06-16 11:51:56 -0400

    various array insert

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function writePassword() {
 
   passwordText.value = password;
 }
+// Various Arrays 
+
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 //GIVEN I need a new, secure password
 //WHEN I click the button to generate a password
 generateBtn.addEventListener("click", writePassword);
@@ -23,6 +30,7 @@ generateBtn.addEventListener("click", writePassword);
 if (passwordLength < 8 || passwordLength > 128) {
   return alert("Enter a character length between 8 and 128.");
 }
+
 //THEN I choose lowercase, uppercase, numeric, and/or special characters
 //WHEN I answer each prompt
 
